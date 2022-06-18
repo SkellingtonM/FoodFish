71d872b8bfc5dfe0af932d84d9530cd7031b5ded
Macklin <macklinlimpan@gmail.com>
2022-06-18 10:10:35 +0800

    Add more

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -96,16 +96,34 @@
       </div>
 
       <!-- SidebarSearch Form -->
-      <div class="form-inline">
-        <div class="input-group" data-widget="sidebar-search">
-          <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
-          <div class="input-group-append">
-            <button class="btn btn-sidebar">
-              <i class="fas fa-search fa-fw"></i>
-            </button>
-          </div>
-        </div>
-      </div>
+      <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+          <li class="nav-item">
+            <a href="{{ route('home') }}" class="nav-link ">
+              <i class="nav-icon fas fa-tachometer-alt"></i>
+              <p>
+                Dashboard
+              </p>
+            </a>
+            
+          </li>
+          <li class="nav-item">
+            <a href="{{route('admin.profiles.index')}}" class="nav-link">
+              <i class="nav-icon fas fa-th"></i>
+              <p>
+                Profile
+              </p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="{{route('admin.prices.index')}}" class="nav-link">
+              <i class="nav-icon fas fa-copy"></i>
+              <p>
+                Price Update
+              </p>
+            </a>
         
           <li class="nav-header">Action</li>
           <li class="nav-item">
@@ -122,7 +140,6 @@
                                     </form>
             
           </li>
-         
           
         </ul>
       </nav>
--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,16 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/profile', [App\Http\Controllers\Admin\ProfileController::class, 'index'])->name('home');
+
+Route::get('/profile', [App\Http\Controllers\Admin\ProfileController::class, 'index'])->name('home');
+
+Route::get('/admin', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::resource('/admin/profiles', Admin\ProfileController::class, ['as'=>'admin']);
+Route::get('/admin/create', 'ProfileController@create');
+Route::post('/profiles/create', 'ProfileController@store');
+Route::resource('/admin/prices', Admin\PricesController::class, ['as'=>'admin']);
+Route::get('/admin/create', 'PricesController@create');
+Route::post('/prices/create', 'PricesController@store');
